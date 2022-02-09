1743cb56d7fced53d3142baf01b60b4e948ca639
williams <oneal.wills@gmail.com>
2022-02-09 20:55:09 +0100

    videocard

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,12 +4,12 @@ import SidebarRow from "./SidebarRow";
 import HomeIcon from "@material-ui/icons/Home";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
-// import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
-// import HistoryIcon from "@material-ui/icons/History";
-// import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
-// import WatchLaterIcon from "@material-ui/icons/WatchLater";
-// import ThumbUpAltOutLinedIcon from "@material-ui/icons/ThumbUpAlt";
-// import ExpandMoreOutLinedIcon from "@material-ui/icons/ExpandMore";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import HistoryIcon from "@material-ui/icons/History";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import ThumbUpAltOutLinedIcon from "@material-ui/icons/ThumbUpAlt";
+import ExpandMoreOutLinedIcon from "@material-ui/icons/ExpandMore";
 
 function Sidebar() {
   return (
@@ -17,14 +17,14 @@ function Sidebar() {
       <SidebarRow selected Icon={HomeIcon} title="Home" />
       <SidebarRow Icon={WhatshotIcon} title="Trending" />
       <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
-      {/* <hr />
+      <hr />
       <SidebarRow Icon={VideoLibraryIcon} title="Library" />
       <SidebarRow Icon={HistoryIcon} title="History" />
       <SidebarRow Icon={OndemandVideoIcon} title="Your Videos" />
       <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
       <SidebarRow Icon={ThumbUpAltOutLinedIcon} title="Liked video" />
       <SidebarRow Icon={ExpandMoreOutLinedIcon} title="Show more" />
-      <hr /> */}
+      <hr />
     </div>
   );
 }
