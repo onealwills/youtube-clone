77103233553a2ab5526e616d90b76611f4224473
williams <oneal.wills@gmail.com>
2022-02-09 20:15:35 +0100

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import "./Sidebar";
+import RecommendedVideos from "./RecommendedVideos";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello world</h1>
+      {/* <h1>Hello world</h1> */}
+
+      {/* header */}
+      <Header />
+
+      <div className="app__page">
+        <Sidebar />
+        <RecommendedVideos />
+      </div>
+
+      {/* sidebar */}
+      {/* recommended video */}
     </div>
   );
 }
