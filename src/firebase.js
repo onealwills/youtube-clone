d80df1cc88dd8e2283d51333b6b92640383fc272
williams <oneal.wills@gmail.com>
2022-02-09 07:33:10 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC0RKrETjycd-txbZ7nLA6Wam5i86TNwts",
+//   authDomain: "clone-26ab4.firebaseapp.com",
+//   projectId: "clone-26ab4",
+//   storageBucket: "clone-26ab4.appspot.com",
+//   messagingSenderId: "977736328860",
+//   appId: "1:977736328860:web:4ed24bb82ddfd2b40def59",
+//   measurementId: "G-RL06E1BGXE",
+// };
